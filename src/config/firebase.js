7ca54c1e0c3406576d3696ef85315c928e153623
Paweł Piotr Owczarek <68189344+p-shepherd@users.noreply.github.com>
2024-01-05 22:03:40 +0100

    Update firebase.js
    deleting api keys

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAICeE7Sj8orulK03ZGFmkRmXojEaovdBs",
-  authDomain: "myfamilytree-c0588.firebaseapp.com",
-  projectId: "myfamilytree-c0588",
-  storageBucket: "myfamilytree-c0588.appspot.com",
-  messagingSenderId: "248444141831",
-  appId: "1:248444141831:web:bdc8a354b8b8997a8b45a2",
-  measurementId: "G-PGRLQYP9Q4",
+
 };
 
 const app = initializeApp(firebaseConfig);
